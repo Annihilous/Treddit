32f69dd39c3123ce282edb7b417dd4e4bfdf82df
Jennifer Krohn <jenniferlynn1207@msn.com>
2015-02-19 20:10:51 -0500

    well formatting and time tags

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-         belongs_to :article
+    belongs_to :article
 
 end
