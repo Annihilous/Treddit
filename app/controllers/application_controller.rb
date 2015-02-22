bd63564eaf8c377303b4cdd2131501ead4dd0e60
Annihilous <benjaminjnorton@gmail.com>
2015-02-22 15:55:33 -0500

    updated applicaiton controller for User:user_handle, added user_handle to new and edit forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,9 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 protected
 
 def configure_permitted_parameters
-  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ roles: [] }, :email, :password, :password_confirmation) }
+  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ roles: [] }, :email, :password, :password_confirmation, :user_handle) }
 
-  devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :avatar) }
+  devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :avatar, :user_handle) }
 end
 
 end
